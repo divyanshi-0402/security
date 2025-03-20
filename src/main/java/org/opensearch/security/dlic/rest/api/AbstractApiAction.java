144dc242a104976f13c490ee5ba30914915cf060
Divyanshi Chouksey <divvv@amazon.com>
2025-03-20 06:11:38 -0700

    milestone1 changes

--- src/main/java/org/opensearch/security/dlic/rest/api/AbstractApiAction.java
+++ src/main/java/org/opensearch/security/dlic/rest/api/AbstractApiAction.java
@@ -339,6 +339,14 @@ public abstract class AbstractApiAction extends BaseRestHandler {
         return ValidationResult.success(securityConfiguration);
     }
 
+    // final void saveOrUpdateConfiguration(
+    //     final Client client,
+    //     final SecurityDynamicConfiguration<?> configuration,
+    //     final OnSucessActionListener<IndexResponse> onSucessActionListener
+    // ) {
+    //     saveAndUpdateConfigsAsync(securityApiDependencies, client, getConfigType(), configuration, onSucessActionListener);
+    // }
+
     final void saveOrUpdateConfiguration(
         final Client client,
         final SecurityDynamicConfiguration<?> configuration,
